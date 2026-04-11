a21b37ea1cf3992dbc617ae33fe551974e59781a
saparboyevmagrur-blip <saparboyevmagrur.com>
2026-04-11 15:25:04 +0500

    practice: python easy algorithms

---
+++ 22.py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+print(a % b == 0 or b % a == 0)
+    
---
+++ 5.py
@@ -0,0 +1,14 @@
+# 1 + 2 + 3 + 4 + ... + n
+# n = int(input())
+# s = 0
+# for number in range(1, n + 1):
+#     s += number # s = s + number
+
+# print(s)
+# import math
+# n = int(input())
+# s = 0
+# for number in range(1, n + 1):
+#     s += int(math.sqrt(number)) # s = s + number
+
+# print(s)
