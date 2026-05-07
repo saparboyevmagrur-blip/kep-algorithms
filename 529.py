c715d1e7ca645d09dfef9369218ba886bbe91c84
saparboyevmagrur-blip <saparboyevmagrur.com>
2026-05-07 16:30:18 +0500

    Add implementations for mapping square values, counting divisors, and filtering even numbers

---
+++ 529.py
@@ -0,0 +1,14 @@
+# map(func, iterable)
+def divisors_count(son):
+    # natural bo'luvchilari: 1, 2, ... 9, ...son
+    count = 0
+    for i in range(1, son + 1):
+        if son % i == 0:
+            count += 1
+
+    return count
+
+def map_divisors_count(sequence):
+    return map(divisors_count, sequence)
+
+print(list(map_divisors_count([-8, 10, 4, 0, 12, -5])))
---
+++ 532.py
@@ -0,0 +1,2 @@
+# homework
+# is_prime degan function yozish kerak
